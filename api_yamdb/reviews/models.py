c2d4464c40a34e7719bdb69c5d045eee9f983d6b
Philipp Pyhonin <caveinfix@gmail.com>
2022-06-22 18:02:27 +0300

    Add models: Category, Title, Genre (migrate)

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -17,3 +17,80 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+from .validators import validate_year
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name="Название категории",
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name="Slug",
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name="Название жанра",
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name="Slug",
+    )
+
+    class Meta:
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(
+        max_length=256,
+        verbose_name="Название произведения",
+    )
+    year = models.IntegerField(
+        validators=[validate_year],
+        verbose_name="Год произведения",
+    )
+    description = models.TextField(
+        max_length=200,
+        blank=True,
+        null=True,
+        verbose_name="Описание произведения",
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        blank=True,
+        related_name="titles",
+        verbose_name="Жанр произведения",
+    )
+    category = models.ForeignKey(
+        Category,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="titles",
+        verbose_name="Категория произведения",
+    )
+
+    class Meta:
+        verbose_name = "Произведение"
+        verbose_name_plural = "Произведения"
+
+    def __str__(self):
+        return self.name
